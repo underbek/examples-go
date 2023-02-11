9f535c291f343da92e942e637d39b8cdb41efd5e
andrey.lukin <l.a.r.p@yandex.ru>
2023-02-11 19:43:21 +0700

    Updated utils

--- testutils/utils.go
+++ testutils/utils.go
@@ -2,6 +2,7 @@ package testutils
 
 import (
 	"encoding/json"
+	"io"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -17,6 +18,12 @@ func jsonMarshal(t *testing.T, data any) string {
 		return v
 	case []byte:
 		return string(v)
+	case io.Reader:
+		body, err := io.ReadAll(v)
+		if err != nil {
+			t.Fatal(err)
+		}
+		return string(body)
 	default:
 		res, err := json.Marshal(v)
 		if err != nil {
